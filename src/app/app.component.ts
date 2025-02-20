6a8d9a08b86b57e1d34e8958498aa38623d5b724
srsexton94 <srsexton94@gmail.com>
2025-02-20 17:46:21 -0500

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,11 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  template: `
+    <h1>Hello {{ title }}!</h1>
+  `
 })
 export class AppComponent {
-  title = 'shop-vibe';
+  title = 'ShopVibe';
 }
