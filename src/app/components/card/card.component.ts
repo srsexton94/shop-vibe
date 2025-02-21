57476222b4356e4c37b8f9f9105a7b13156a9b57
srsexton94 <srsexton94@gmail.com>
2025-02-20 22:42:41 -0500

    style card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,19 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../../models/products.model';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   styleUrl: './card.component.scss',
   template: `
     <div class="card">
-      <h2>{{ product().title }}</h2>
+      <img [src]="product().image" [alt]="product().title" />
+      <div class="content">
+        <h2>{{ product().title }}</h2>
+        <p>{{ '$' + product().price.toFixed(2) }}</p>
+        <app-primary-button label="Add to cart" />
+      </div>
     </div>
   `,
 })
