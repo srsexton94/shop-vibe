d76e974f27240d3d4cae69717f19cea824447571
srsexton94 <srsexton94@gmail.com>
2025-02-21 15:46:39 -0500

    change html order of stock

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -8,19 +8,21 @@ import { PrimaryButtonComponent } from '../primary-button/primary-button.compone
   styleUrl: './card.component.scss',
   template: `
     <div class="card">
-      <p class="stock">
-        @if (product().stock) {
-          {{ product().stock }} left
-        } @else {
-          <span class="out">Out of Stock</span>
-        }
-      </p>
       <img [src]="product().image" [alt]="product().title" />
       <div class="content">
         <h2>{{ product().title }}</h2>
         <p>{{ '$' + product().price.toFixed(2) }}</p>
-        <app-primary-button label="Add to cart" [isDisabled]="!product().stock" />
+        <app-primary-button
+          label="Add to cart"
+          [isDisabled]="!product().stock"
+        />
       </div>
+      <p class="stock">
+        @if (product().stock) {
+        {{ product().stock }} left } @else {
+        <span class="out">Out of Stock</span>
+        }
+      </p>
     </div>
   `,
 })
